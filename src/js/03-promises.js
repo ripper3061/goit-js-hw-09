89f86e2c7f6bcc2ebf36328fab3c45ad71407a1b
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-28 21:38:21 +0300

    правки + вариант через map вместо for

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,3 @@
-import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
@@ -10,6 +9,7 @@ function submitForm(event) {
   let delay = Number(form.delay.value);
   const step = Number(form.step.value);
   let amount = Number(form.amount.value);
+
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
@@ -21,6 +21,20 @@ function submitForm(event) {
     delay += step;
   }
 }
+// -----------Вариант через map---------------
+//   const promises = new Array(amount).fill(1);
+//   promises.map((_, position) => {
+//     console.log(position);
+//     createPromise(position + 1, delay)
+//       .then(({ position, delay }) => {
+//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//     delay += step;
+//   });
+// }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
