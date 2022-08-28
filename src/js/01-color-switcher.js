144bd27cf64c83a4da5a01a10c95f4930169541d
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-28 13:12:14 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startCS = document.querySelector('button[data-start]');
+const stopCS = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
+let isActive = false;
 
+startCS.addEventListener('click', onStartCS);
+
+stopCS.addEventListener('click', onStopCS);
+
+function onStartCS() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(setBcgColor, 1000);
+}
+
+function onStopCS() {
+  isActive = false;
+  clearInterval(intervalId);
+}
+
+function setBcgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
